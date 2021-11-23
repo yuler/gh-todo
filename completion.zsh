39651ccc97b8225ea4e109e998af7e8d220c0f9e
YuLe <joyful4yu@gmail.com>
2021-11-23 16:21:15 +0800

    feat: define completion.zsh file (#10)

---
+++ completion.zsh
@@ -0,0 +1,30 @@
+#compdef gh-todo
+
+_gh-todo() {
+    local line state
+
+    _arguments -C \
+        "--help[Show help for command]" \
+        "--version[Show version]" \
+        "1: :->cmds" \
+        "*::arg:->args"
+
+    case "$state" in
+    cmds)
+        _values "Actions:" \
+            "init[Create \`todo\` repo]" \
+            "home[Open \`issues\` in browser]" \
+            "add[Add todo item]" \
+            "edit[Open \`issue\` in browser]" \
+            "done[Close \`issue\`]" \
+            "list[Show todo list]"
+        ;;
+    args)
+        case $line[1] in
+        init)
+            _arguments '(--template)--template[template repo]'
+            ;;
+        esac
+        ;;
+    esac
+}
